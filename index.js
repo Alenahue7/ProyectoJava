324492e835c988c9eeec4d66985a49ab7222e506
alenahue <ngaguirre93@hotmail.com>
2023-12-22 20:04:43 -0300

    Segunda preentrega

--- index.js
+++ index.js
@@ -1,47 +1,90 @@
-	//Primera preentrega. Alejandro Nahuel Garcia
-	//Para ver mi proyecto, he configurado un servidor local para ver mi pagina (Phyton) mediante la siguiente sentencia: python -m http.server
-	//http://localhost:8000/
+	//Segunda preentrega. Alejandro Nahuel Garcia
+	
+  <!DOCTYPE html>
+<html lang="es">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <title>Calculadora</title>
+</head>
+<body>
+  <script>
+    // Declaro un objeto para manejar la calculadora
+    let calculadora = {
+      numero1: 0,
+      numero2: 0,
+      operacion: "",
+      resultado: 0,
 
-	// Declaro las variables con let
-let numero1, numero2, operacion, resultado;
+      // Este es el método para realizar operaciones matemáticas
+      calcularOperacion: function() {
+        switch (this.operacion) {
+          case 'suma':
+            return this.numero1 + this.numero2;
+          case 'resta':
+            return this.numero1 - this.numero2;
+          case 'multiplicacion':
+            return this.numero1 * this.numero2;
+          case 'division':
+            return this.numero1 / this.numero2;
+          default:
+            return 'Operación no válida';
+        }
+      },
 
-// Hago una funcion para realizar operaciones matemáticas
-function calcularOperacion(num1, num2, operador) {
-  switch (operador) {
-    case 'suma':
-      return num1 + num2;
-    case 'resta':
-      return num1 - num2;
-    case 'multiplicacion':
-      return num1 * num2;
-    case 'division':
-      return num1 / num2;
-    default:
-      return 'Operación no válida';
-  }
-}
+      // Este es el metodo para solicitar números al usuario
+      pedirNumeros: function() {
+        this.numero1 = this.obtenerNumero("Ingrese el primer número:");
+        this.numero2 = this.obtenerNumero("Ingrese el segundo número:");
+      },
 
-// Solicito al usuario que ingrese dos números
-numero1 = parseFloat(prompt("Ingrese el primer número:"));
-numero2 = parseFloat(prompt("Ingrese el segundo número:"));
+      // Este es el metodo para solicitar la operación al usuario
+      pedirOperacion: function() {
+        this.operacion = prompt("Elija una operación: suma, resta, multiplicacion, division");
+      },
 
-// Solicito al usuario que elija una operación
-operacion = prompt("Elija una operación: suma, resta, multiplicacion, division");
+      // Este es el metodo para mostrar el resultado en un alert y en la consola
+      mostrarResultado: function() {
+        alert("El resultado de la " + this.operacion + " es: " + this.resultado);
+        console.log("Número 1: " + this.numero1);
+        console.log("Número 2: " + this.numero2);
+        console.log("Operación: " + this.operacion);
+        console.log("Resultado: " + this.resultado);
+      },
 
-// Validacion de la operación ingresada
-if (['suma', 'resta', 'multiplicacion', 'division'].includes(operacion)) {
-  // Calcular el resultado utilizando la función
-  resultado = calcularOperacion(numero1, numero2, operacion);
+      // Este es el metodo para validar y obtener un número del usuario
+      obtenerNumero: function(mensaje) {
+        let input;
+        do {
+          input = prompt(mensaje);
+        } while (!this.esNumeroValido(input));
+        return parseFloat(input);
+      },
 
-  // Muestro el resultado en un alert
-  alert("El resultado de la " + operacion + " es: " + resultado);
+      // Función para validar si es un número válido
+      esNumeroValido: function(valor) {
+        return !isNaN(parseFloat(valor)) && isFinite(valor);
+      }
+    };
 
-  // Imprimo la información en la consola
-  console.log("Número 1: " + numero1);
-  console.log("Número 2: " + numero2);
-  console.log("Operación: " + operacion);
-  console.log("Resultado: " + resultado);
-} else {
-  // Si la operación no es válida, se debe mostrar un mensaje de error
-  alert("Operación no válida. Por favor, elija una operación válida.");
-}
+    // Solicito al usuario que ingrese números y elija una operación
+    calculadora.pedirNumeros();
+    calculadora.pedirOperacion();
+
+    // Validación de la operación ingresada
+    if (['suma', 'resta', 'multiplicacion', 'division'].includes(calculadora.operacion)) {
+      // Ahora calculo el resultado utilizando el método de la calculadora
+      calculadora.resultado = calculadora.calcularOperacion();
+
+      // Aqui muestro el resultado utilizando el método de la calculadora
+      calculadora.mostrarResultado();
+    } else {
+      // Si la operación no es válida, se debe mostrar un mensaje de error
+      alert("Operación no válida. Por favor, elija una operación válida.");
+    }
+  </script>
+</body>
+</html>
+
+
+
